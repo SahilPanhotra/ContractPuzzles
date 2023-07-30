e1d268eab6f200a4215901f20a2357877621f506
SahilPanhotra <sahilpanhotra@gmail.com>
2023-07-30 17:51:18 +0530

    completed all challenges

--- test/game5Test.js
+++ test/game5Test.js
@@ -2,18 +2,44 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
 
 describe('Game5', function () {
+  var winner;
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
+    const signers = await ethers.getSigners();
+    const winner = await getWinner(signers[0]);
 
-    return { game };
+    return { game ,winner};
   }
+
+  async function getWinner(signer) {
+    var threshold = '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf';
+    while (!winner) {
+      // create random addresses until the address that has value less than the threshold is found.
+      const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      const address = await wallet.getAddress();
+
+      if (address < threshold) {
+        // send the address 1 ether to ensure that it has some funds to call the win function.
+
+        await signer.sendTransaction({
+          value: ethers.utils.parseEther("1.0"),
+          to: address
+        })
+
+        winner = wallet;
+      }
+    }
+
+    return winner;
+  }
+
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(winner).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
